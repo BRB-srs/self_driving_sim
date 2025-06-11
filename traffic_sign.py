9259eebfefb3c6aaab17f4007d605dd3f51dd5c1
BRB-srs <rosemaryboadiwaaboadi@gmail.com>
2025-06-11 11:08:42 +0000

    Update traffic_sign.py

--- traffic_sign.py
+++ traffic_sign.py
@@ -1,12 +1,1 @@
 
-class TrafficSign:
-    def __init__(self, color):
-        self.color = color.lower()
-
-    def get_direction(self):
-        if self.color == "red":
-            return "Pass on the RIGHT"
-        elif self.color == "green":
-            return "Pass on the LEFT"
-        else:
-            return "Invalid traffic sign color. Only red or green are allowed."
